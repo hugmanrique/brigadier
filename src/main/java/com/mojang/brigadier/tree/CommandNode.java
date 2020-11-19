0ed84168e7a52a65acf518f4470f6029160bb9fe
Andrew Steinborn <git@steinborn.me>
2020-11-19 11:41:14 -0500

    Store children in TreeMap instead.

--- src/main/java/com/mojang/brigadier/tree/CommandNode.java
+++ src/main/java/com/mojang/brigadier/tree/CommandNode.java
@@ -20,11 +20,12 @@ import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Predicate;
 
 public abstract class CommandNode<S> implements Comparable<CommandNode<S>> {
-    private final Map<String, CommandNode<S>> children = new LinkedHashMap<>();
+    private final Map<String, CommandNode<S>> children = new TreeMap<>();
     private final Map<String, ArgumentCommandNode<S, ?>> arguments = new LinkedHashMap<>();
     private final Predicate<S> requirement;
     private final CommandNode<S> redirect;
